e199cbf9456af29be6acd0f227d1a8d4b413541d
Nicochou <nicolas.boulein@gmail.com>
2019-06-02 15:14:07 +0200

    up dev

--- application/controllers/Game.php
+++ application/controllers/Game.php
@@ -33,37 +33,50 @@ class Game extends CI_Controller {
 		$this->load->view('games/clean_art');
 		$timeCleanArt = $this->input->post('time');
 
-		//$this->session->set_userdata('time1', $timeCleanArt);
+		$this->session->set_userdata('score', $timeCleanArt);
 	}
 	public function roulette_art()
 	{
 		$this->load->view('games/roulette_art');
 		$timeRo = $this->input->post('time');
+
+
+		$this->session->set_userdata('score', $timeRo + $this->session->userdata('score'));
 	}
 
 	public function color_art()
 	{
 		$this->load->view('games/color_art');
 		$timeCo = $this->input->post('time');
-		$pointsCo = $this->input->post('points');
+
+		$this->session->set_userdata('score', $timeCo + $this->session->userdata('score'));
 	}
   
 	public function puzzle_art()
 	{
 		$this->load->view('games/puzzle_art');
 		$timePu = $this->input->post('time');	
+
+		$this->session->set_userdata('score', $timePu + $this->session->userdata('score'));
 	}
 
 	public function emotion_art()
 	{
 		$this->load->view('games/emotion_art');
 		$timeEm = $this->input->post('time');
+
+		$this->session->set_userdata('score', $timeEm + $this->session->userdata('score'));
 	}
 
 	public function score_final()
 	{	
 		
-		//$data = $this->session->userdata();
+		$data = array
+		(
+			'score' => $this->session->userdata('score')
+		);
+
 		$this->load->view('games/score_final',$data);
 	}
 }
+
